c3d7363b660047edee467eb241751670fe791c11
yutaro201 <20026184@mmm.muroran-it.ac.jp>
2023-05-16 17:52:58 +0900

    完成

--- src/main/java/jp/sagalab/b4zemi/BezierCurve.java
+++ src/main/java/jp/sagalab/b4zemi/BezierCurve.java
@@ -5,20 +5,56 @@ import java.util.List;
 public class BezierCurve {
     private final List<Point> m_controlPoints;
 
+    /**
+     * 制御点列を指定してBezier曲線オブジェクトを生成する
+     * @param _controlPoints 制御点列
+     */
     private BezierCurve(List<Point> _controlPoints){
         m_controlPoints = _controlPoints;
     }
 
+    /**
+     * Bezier曲線の生成を行うためのstaticファクトリーメソッド
+     * @param _controlPoints 制御点列
+     * @return Bezier曲線のインスタンス
+     */
     public static BezierCurve create(List<Point> _controlPoints){
         return new BezierCurve(_controlPoints);
     }
 
+    /**
+     * 制御点列のコピーを取得する
+     * @return 制御点列のコピー
+     */
     public List<Point> getControlPoints(){
         return new ArrayList<Point>(m_controlPoints);
     }
 
+    /**
+     * 次数を取得する
+     * @return 次数
+     */
     public int getDegree(){
         return m_controlPoints.size()-1;
     }
-    
+
+    /**
+     * パラメータ t に対応する評価点を De Casteljau のアルゴリズムで評価する
+     * @param _t 閉区間 [ 0, 1 ] 内のパラメータ
+     * @return パラメータ t に対応する評価点
+     */
+    public Point evaluate(double _t){
+        if(m_controlPoints.size()==1){
+            return m_controlPoints.get(0);
+        }
+        else {
+            List<Point> dividePoints = new ArrayList<>();
+            for (int n = 0; n < m_controlPoints.size()-1; n++) {
+                Point i;
+                i = m_controlPoints.get(n).divide(m_controlPoints.get(n + 1), _t);
+                dividePoints.add(i);
+            }
+            return BezierCurve.create(dividePoints).evaluate(_t);
+        }
+    }
 }
--- src/main/java/jp/sagalab/b4zemi/Point.java
+++ src/main/java/jp/sagalab/b4zemi/Point.java
@@ -1,24 +1,55 @@
 package jp.sagalab.b4zemi;
 
+/**
+ * 平面上の点を表す．平面上の点のｘ座標とy座標を保持するクラス
+ * @author Nirei Yutaro
+ */
 public class Point {
     private final double m_x;
     private final double m_y;
 
+    /**
+     * 指定した座標にある点を生成する
+     * @param _x x座標
+     * @param _y y座標
+     */
     private Point(double _x, double _y){
         m_x = _x;
         m_y = _y;
     }
 
+    /**
+     * 点の生成を行うためのstaticファクトリーメソッド
+     * @param _x x座標
+     * @param _y y座標
+     * @return 点のインスタンス
+     */
     public static Point create(double _x, double _y){
         return new Point(_x,_y);
     }
 
+    /**
+     * 点のx座標を取得する
+     * @return x座標
+     */
     public double getX(){
         return m_x;
     }
+
+    /**
+     * 点のx座標を取得する
+     * @return y座標
+     */
     public double getY(){
         return m_y;
     }
+
+    /**
+     * この点ともう一つの点を t : ( 1 - t ) で内分 ( t < 0 または t > 1 の場合は外分 ) する
+     * @param _other もう一つの点
+     * @param t 比のパラメータ
+     * @return 内分点（外分点）
+     */
     public Point divide(Point _other, double t) {
         double otherX;
         double otherY;
