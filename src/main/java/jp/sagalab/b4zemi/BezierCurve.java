a8ccc8d16aeb2895e31635348e97c40ced09f345
nakajima0120 <21026162@mmm.muroran-it.ac.jp>
2023-07-18 17:42:57 +0900

    完成

--- src/main/java/jp/sagalab/b4zemi/BezierCurve.java
+++ src/main/java/jp/sagalab/b4zemi/BezierCurve.java
@@ -30,7 +30,13 @@ public class BezierCurve {
      */
     public double factorial(double n){
         //階乗を計算する処理を記述してください
-        return 0;
+        double x = 1;
+        int i =1;
+        while(i <= n) {
+            x = x * i;
+            i++;
+        }
+        return x;
     }
 
     /**
@@ -41,7 +47,9 @@ public class BezierCurve {
      */
     public double Bernstein(int _i,double _t){
         //Bernstein多項式Bの計算する処理を記述してください
-        return 0;
+        double a = factorial(m_controlPoints.size() - 1) / (factorial(_i) * factorial((m_controlPoints.size() - 1) - _i));
+        double B = a * Math.pow(_t, _i) * Math.pow(1 - _t, m_controlPoints.size() - 1 - _i);
+        return B;
     }
 
     /**
@@ -52,7 +60,15 @@ public class BezierCurve {
     public Point evaluate(double _t){
         //Bernstein多項式を用いてBernstein多項式表現の計算処理を記述してください
         //Point.javaにあるcreateメソッドを使えばポイントのインスタンスを作ることが出来ます。
-        return null;
+        double sumx = 0;
+        double sumy = 0;
+
+        for(int i = 0; i <= m_controlPoints.size() - 1; i++){
+            sumx += (m_controlPoints.get(i).getX()) * Bernstein(i, _t);
+            sumy += (m_controlPoints.get(i).getY()) * Bernstein(i, _t);
+        }
+
+        return Point.create(sumx, sumy);
     }
 
 }
